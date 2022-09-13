954c5a66af840c8d602851134fdb7cf023a1ff5a
Glebovski <andrewglebovski@gmail.com>
2022-09-13 10:51:33 +0300

    Add back compare
    Добавил сортировку строки с конца

--- src/onegin.cpp
+++ src/onegin.cpp
@@ -5,13 +5,14 @@
 
 #include <stdlib.h>
 #include <ctype.h>
+#include <string.h>
 #include "onegin.hpp"
 
 
 /**
  * \brief Get next alpha symbol in string
  * \param [in] str String to search alpha in
- * \return Pointer to first alpha element
+ * \return Pointer to the first element found
 */
 const char *get_next_alpha(const char *str);
 
@@ -22,10 +23,30 @@ const char *get_next_alpha(const char *str);
  * \param [in] ptrB The second element to compare
  * \return Standart compare function output
  * 
- * This function is used in sort_poem() as compare function. 
+ * \note This function is used in sort_poem() as compare function. 
  * So it takes pointers to elements of array of strings
 */
 int front_compare(const void *ptrA, const void *ptrB);
+
+
+/**
+ * \brief Get previous alpha symbol in string
+ * \param [in] str String to search alpha in
+ * \return Pointer to the first element found
+*/
+const char *get_prev_alpha(const char *str);
+
+
+/**
+ * \brief Compares two strings using get_prev_alpha() (comparing starts from end)
+ * \param [in] ptrA The first element to compare
+ * \param [in] ptrB The second element to compare
+ * \return Standart compare function output
+ * 
+ * This function is used in sort_poem() as compare function. 
+ * So it takes pointers to elements of array of strings
+*/
+int back_compare(const void *ptrA, const void *ptrB);
 
 
 const char *get_next_alpha(const char *str) {
@@ -56,6 +77,36 @@ int front_compare(const void *ptrA, const void *ptrB) {
 }
 
 
+const char *get_prev_alpha(const char *str) {
+    while (!isalpha(*str--)) {
+        if (*str == '\0') 
+            return str;
+    }
+    
+    return str + 1;
+}
+
+
+int back_compare(const void *ptrA, const void *ptrB) {
+    const char *a = *(const char **) ptrA, *b = *(const char **) ptrB;
+    a += strlen(a);
+    b += strlen(b);
+    
+    while (1) {
+        a = get_prev_alpha(a);
+        b = get_prev_alpha(b);
+
+        if (*a == '\0' || *b == '\0')
+            break;
+
+        if (*a-- != *b--)
+            break;
+    }
+
+    return *(a + 1) - *(b + 1);
+}
+
+
 void sort_poem(char *poem[], unsigned int poem_size) {
-    qsort(poem, poem_size, sizeof(*poem), &front_compare);
+    qsort(poem, poem_size, sizeof(*poem), &back_compare);
 }
