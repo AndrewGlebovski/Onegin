81f6cda43ce7f7ab1ad47149e3e45aa5acba544d
Glebovski <andrewglebovski@gmail.com>
2022-09-23 15:37:03 +0300

    Add replace_in_buffer
    Добавил функцию которая заменяет один символ в буфере на другой

--- src/io.cpp
+++ src/io.cpp
@@ -15,50 +15,53 @@
 size_t get_file_size(FILE *stream);
 
 
+/**
+ * \brief Replaces specific character in string 
+ * \param [out] buffer Array to change
+ * \param [in] old_char This symbol will be replaced
+ * \param [in] new_char This symbol will be set
+ * \return Count of replacement
+*/
+int replace_in_buffer(char *buffer, const char old_char, const char new_char);
+
+
 int read_viewer(StringViewer *viewer, FILE *stream) {
     ASSERT_AND_LOG(viewer, INVALID_ARGUMENT, "StringViewer was NULL", return INVALID_ARGUMENT);
     ASSERT_AND_LOG(stream, INVALID_ARGUMENT, "File was NULL",         return INVALID_ARGUMENT);
 
     size_t size = get_file_size(stream);
 
-    char *storage = (char *) calloc(size + 1, sizeof(char));
-    ASSERT_AND_LOG(storage, ALLOCATE_FAIL, "Not enough memory for text", return ALLOCATE_FAIL);
+    char *text = (char *) calloc(size + 1, sizeof(char));
+    ASSERT_AND_LOG(text, ALLOCATE_FAIL, "Not enough memory for text", return ALLOCATE_FAIL);
 
-    ASSERT_AND_LOG(!setvbuf(stream, NULL, _IOFBF, size), BUFFER_ERROR, "Set buffer to file size returns error", free(storage); viewer -> status = FREE; return BUFFER_ERROR);
+    ASSERT_AND_LOG(!setvbuf(stream, NULL, _IOFBF, size), BUFFER_ERROR, "Set buffer to file size returns error", free(text); viewer -> status = FREE; return BUFFER_ERROR);
 
-    size = fread(storage, sizeof(char), size, stream);
-    ASSERT_AND_LOG(!ferror(stream), FILE_READ_ERROR, "Error while reading file", free(storage); viewer -> status = FREE; return FILE_READ_ERROR);
+    size = fread(text, sizeof(char), size, stream);
+    ASSERT_AND_LOG(!ferror(stream), FILE_READ_ERROR, "Error while reading file", free(text); viewer -> status = FREE; return FILE_READ_ERROR);
 
-    ASSERT_AND_LOG(!setvbuf(stream, NULL, _IONBF, 0), BUFFER_ERROR, "Set buffer 0 returns error", free(storage); viewer -> status = FREE; return BUFFER_ERROR);
+    ASSERT_AND_LOG(!setvbuf(stream, NULL, _IONBF, 0), BUFFER_ERROR, "Set buffer 0 returns error", free(text); viewer -> status = FREE; return BUFFER_ERROR);
 
-    storage = (char *) realloc(storage, size + 1);
-    ASSERT_AND_LOG(storage, ALLOCATE_FAIL, "Reallocate text fail", return ALLOCATE_FAIL);
+    text = (char *) realloc(text, size + 1);
+    ASSERT_AND_LOG(text, ALLOCATE_FAIL, "Reallocate text fail", return ALLOCATE_FAIL);
 
-    int lines = 1;
-
-    storage[size] = '\0';
-    for(size_t i = 0; i < size; i++) {
-        if (storage[i] == '\n') {
-            storage[i] = '\0';
-            lines++;
-        }
-    }
+    text[size] = '\0';
+    int lines = 1 + replace_in_buffer(text, '\n', '\0');
 
     viewer -> lines = (String *) calloc(lines + 1, sizeof(String));
 
-    ASSERT_AND_LOG(viewer -> lines, ALLOCATE_FAIL, "Not enough memory for lines", free(storage); viewer -> status = FREE; return ALLOCATE_FAIL);
+    ASSERT_AND_LOG(viewer -> lines, ALLOCATE_FAIL, "Not enough memory for lines", free(text); viewer -> status = FREE; return ALLOCATE_FAIL);
 
-    viewer -> text = storage;
+    viewer -> text = text;
 
     for(int l = 0; l < lines; l++) {
-        (viewer -> lines)[l] = {storage, 0};
+        (viewer -> lines)[l] = {text, 0};
 
-        while(*storage != '\0')
-            storage++;
+        while(*text != '\0')
+            text++;
 
-        (viewer -> lines)[l].len = (int)(storage - (viewer -> lines)[l].str);
+        (viewer -> lines)[l].len = (int)(text - (viewer -> lines)[l].str);
 
-        storage++;
+        text++;
     }
 
     (viewer -> lines)[lines] = {nullptr, -1};
@@ -67,6 +70,20 @@ int read_viewer(StringViewer *viewer, FILE *stream) {
     viewer -> status = FILL;
 
     return OK;
+}
+
+
+int replace_in_buffer(char *buffer, const char old_char, const char new_char) {
+    ASSERT_AND_LOG(buffer, INVALID_ARGUMENT, "Buffer was NULL", return -1);
+
+    int count = 0;
+    for(size_t i = 0; buffer[i]; i++) {
+        if (buffer[i] == old_char) {
+            buffer[i] = new_char;
+            count++;
+        }
+    }
+    return count;
 }
 
 
